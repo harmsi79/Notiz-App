60083bae5f14ce85e97a54504cf8d5ca1b7aef47
harmsi79 <andreas.harms1979@gmail.com>
2023-12-19 16:06:41 +0100

    JS hinzugefuegt

--- app.js
+++ app.js
@@ -1,57 +1,137 @@
+const saveButtonEl = document.querySelector(".save-note");
+const deleteButtonEl = document.querySelector(".delete-note");
+const newNoteButtonEl = document.querySelector(".create-new");
 const notesListEl = document.querySelector(".notes-list");
+const titleInputEl = document.getElementById("title-input");
+const contentInputEl = document.getElementById("content-input");
 
-notesListEl.innerHTML = "Hallo 123";
+saveButtonEl.addEventListener("click", clickSaveButton);
+deleteButtonEl.addEventListener("click", clickDeleteButton);
+newNoteButtonEl.addEventListener("click", newNote);
 
-const MOCK_NOTES = [
-  {
-    id: 1,
-    title: "Notiz 1",
-    content: "12345",
-    lastUpdated: new Date().getTime(),
-  },
-  {
-    id: 2,
-    title: "Notiz 2",
-    content: "12345",
-    lastUpdated: new Date().getTime(),
-  },
-  {
-    id: 3,
-    title: "Notiz3",
-    content: "12345",
-    lastUpdated: new Date().getTime(),
-  },
-  {
-    id: 4,
-    title: "Notiz 4",
-    content: "12345",
-    lastUpdated: new Date().getTime(),
-  },
-  {
-    id: 5,
-    title: "Notiz 5",
-    content: "12345",
-    lastUpdated: new Date().getTime(),
-  },
-];
+displayNotesList();
+applyListeners();
+
+function applyListeners() {
+  const noteEntriesEls = document.querySelectorAll(".note-entry");
+
+  noteEntriesEls.forEach((noteEntry) => {
+    noteEntry.addEventListener("click", () =>
+      selectNote(noteEntry.getAttribute("data-id"))
+    );
+  });
+}
 
 function displayNotesList() {
-  const notes = MOCK_NOTES;
+  const notes = getNotes();
+
+  const sortedNotes = notes.sort(
+    (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated
+  );
 
   let html = "";
 
-  notes.forEach((note) => {
+  // TODO: escape html for title and content <- UNBEDINGT INS TUTORIAL DEMONSTIEREN
+  sortedNotes.forEach((note) => {
     html += `
-        <div class="note-entry" data-id="${note.id}">
-         <div class="note-title">${note.title}</div>
-            <div class="note-teaser">
-            ${note.content}
-         </div>
-         <div class="note-date">${note.lastUpdated}</div>
-         </div>`;
+            <div class="note-entry" data-id="${note.id}">
+              <div class="note-title">${escapeHtml(note.title)}</div>
+              <div class="note-content-teaser">${escapeHtml(note.content)}</div>
+              <div class="note-date">${new Date(
+                note.lastUpdated
+              ).toLocaleString("de-DE")}</div>
+            </div>`;
   });
 
   notesListEl.innerHTML = html;
 }
 
-displayNotesList();
+function clickSaveButton() {
+  const title = titleInputEl.value;
+  const content = contentInputEl.value;
+
+  if (!title || !content) {
+    alert("Bitte Titel und Inhalt eingeben");
+    return;
+  }
+
+  let currentId = undefined;
+
+  const currentlySelectedNote = getCurrentlySelectedNote();
+
+  if (currentlySelectedNote)
+    currentId = currentlySelectedNote.getAttribute("data-id");
+
+  saveNote(title, content, Number(currentId));
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  displayNotesList();
+  applyListeners();
+}
+
+function clickDeleteButton() {
+  const currentlySelectedNote = getCurrentlySelectedNote();
+
+  if (!currentlySelectedNote) return;
+
+  const currentId = getCurrentlySelectedNote().getAttribute("data-id");
+
+  if (!currentId) return;
+
+  deleteNote(currentId);
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  displayNotesList();
+  applyListeners();
+}
+
+function selectNote(id) {
+  const selectedNoteEl = document.querySelector(`.note-entry[data-id="${id}"]`);
+
+  if (selectedNoteEl.classList.contains("selected-note")) return;
+
+  removeSelectedClassFromAllNotes();
+
+  selectedNoteEl.classList.add("selected-note");
+
+  const notes = getNotes();
+
+  const selectedNote = notes.find((note) => note.id === Number(id));
+
+  if (!selectedNote) return;
+
+  titleInputEl.value = selectedNote.title;
+  contentInputEl.value = selectedNote.content;
+}
+
+function removeSelectedClassFromAllNotes() {
+  const noteEntries = document.querySelectorAll(".note-entry");
+
+  noteEntries.forEach((noteEntry) => {
+    noteEntry.classList.remove("selected-note");
+  });
+}
+
+function newNote() {
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  removeSelectedClassFromAllNotes();
+}
+
+function getCurrentlySelectedNote() {
+  return document.querySelector(".selected-note");
+}
+
+function escapeHtml(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
---
+++ notesApi.js
@@ -0,0 +1,57 @@
+const LOCAL_STORAGE_KEY = "notizapp-notizen";
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+}
+
+function getNextId() {
+  const notes = getNotes();
+
+  const sortedNotes = notes.sort((noteA, noteB) => noteA.id - noteB.id);
+
+  let nextId = 1;
+
+  for (let note of sortedNotes) {
+    if (nextId < note.id) break;
+
+    nextId = note.id + 1;
+  }
+
+  return nextId;
+}
+
+function saveNote(title, content, id = undefined) {
+  const notes = getNotes();
+
+  if (!id) {
+    notes.push({
+      title,
+      content,
+      id: getNextId(),
+      lastUpdated: new Date().getTime(),
+    });
+  } else {
+    const indexOfNoteWithId = notes.findIndex((note) => note.id === id);
+
+    if (indexOfNoteWithId > -1) {
+      notes[indexOfNoteWithId] = {
+        title,
+        content,
+        id,
+        lastUpdated: new Date().getTime(),
+      };
+    }
+  }
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+}
+
+function deleteNote(id) {
+  if (!id) return;
+
+  const notes = getNotes();
+
+  const filteredNotes = notes.filter((note) => note.id !== Number(id));
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(filteredNotes));
+}
